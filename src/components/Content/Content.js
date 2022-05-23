266498884d54377bd2af7c74fd392809283b8de6
Andrey <kasyan201999@gmail.com>
2022-05-23 12:39:22 +0300

    react lazy

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import LoginPage from '../Login/Login';
+import Preloader from '../common/Preloader';
+// import LoginPage from '../Login/Login';
 import styles from './Content.module.css';
-import MessagesContainer from './Messages/MessagesContainer';
+// import MessagesContainer from './Messages/MessagesContainer';
 import ProfileContainer from './Profile/ProfileContainer';
-import UsersContainer from './Users/UsersContainer';
+// import UsersContainer from './Users/UsersContainer';
 
+const MessagesContainer = React.lazy(() => import('./Messages/MessagesContainer'))
+const UsersContainer = React.lazy(() => import('./Users/UsersContainer'))
+const LoginPage = React.lazy(() => import('../Login/Login'))
 
 function Content() {
    return (
       <main className={styles.content}>
-         <Routes>
-            <Route path="/profile/" element={<ProfileContainer />}>
-            </Route>
-            <Route path="/profile/">
-               <Route path=":userId" element={<ProfileContainer />} />
-            </Route>
-            <Route path="/messages/*" element={<MessagesContainer />} />
-            <Route path="/users/*" element={<UsersContainer />} />
-            <Route path="/login/*" element={<LoginPage />} />
-         </Routes>
+         <Suspense fallback={<Preloader />}>
+            <Routes>
+               <Route path="/profile/" element={<ProfileContainer />}>
+               </Route>
+               <Route path="/profile/">
+                  <Route path=":userId" element={<ProfileContainer />} />
+               </Route>
+               <Route path="/messages/*" element={<MessagesContainer />} />
+               <Route path="/users/*" element={<UsersContainer />} />
+               <Route path="/login/*" element={<LoginPage />} />
+            </Routes>
+         </Suspense>
       </main>
    )
 }
